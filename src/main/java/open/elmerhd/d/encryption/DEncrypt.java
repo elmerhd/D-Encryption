55cfdbc21dc4069bf8817dc470f63060e8f27076
Elmer Duron <elmerduron2016@gmail.com>
2020-02-24 18:50:49 +0800

    install web laf ui

--- src/main/java/open/elmerhd/d/encryption/DEncrypt.java
+++ src/main/java/open/elmerhd/d/encryption/DEncrypt.java
@@ -1,18 +1,20 @@
 package open.elmerhd.d.encryption;
 
-import com.sun.java.swing.plaf.windows.WindowsLookAndFeel;
+import com.alee.laf.WebLookAndFeel;
+import com.alee.skin.dark.WebDarkSkin;
+import java.io.IOException;
 import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
 
 /**
  *
  * @author elmerhd
  */
 public class DEncrypt {
+
     public static void main(String[] args) {
+        WebLookAndFeel.install(WebDarkSkin.class);
         try {
-            UIManager.setLookAndFeel(new WindowsLookAndFeel());
             CommonUtils.createDBFile();
             CommonUtils.createEncryptedFolder();
             SwingUtilities.invokeLater(() -> {
@@ -20,9 +22,8 @@ public class DEncrypt {
                 main.setVisible(true);
                 main.setLocationRelativeTo(null);
             });
-        } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, e.getCause(), "Unable to launch Application", JOptionPane.ERROR_MESSAGE);
-            System.exit(0);
+        } catch (IOException ex) {
+            JOptionPane.showMessageDialog(null, ex.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
         }
     }
 }
